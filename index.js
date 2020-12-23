2c60ecd32f38c971e5c2a1641e160314ba5b095d
JaakkoP1998 <58140616+JaakkoP1998@users.noreply.github.com>
2020-12-23 11:31:21 +0200

    Osan 3 tehtävät

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
+require('dotenv').config()
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
 const app = express()
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Matti Makinen",
-    number: "45-6912441"
-  },
-  {
-    id: 3,
-    name: "Dan Danikton",
-    number: "66-12351424"
-  },
-  {
-    id: 4,
-    name: "Mr Krabs",
-    number: "040-098765"
-  }
+    {
+        id: 1,
+        name: 'Arto Hellas',
+        number: '040-123456'
+    },
+    {
+        id: 2,
+        name: 'Matti Makinen',
+        number:'45-6912441'
+    },
+    {
+        id: 3,
+        name: 'Dan Danikton',
+        number:'66-12351424'
+    },
+    {
+        id: 4,
+        name: 'Mr Krabs',
+        number: '040-098765'
+    }
 ]
 
 //Aloitus sivu http://localhost:3001/
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+    res.send('<h1>Hello World!</h1>')
 })
 
 //http://localhost:3001/api/persons
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
+app.get('/api/persons', (request, response) => {
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 //http://localhost:3001/api/persons/ - Yksittäisen henkilön tiedot
-app.get('/api/persons/:id', (request, response) => {
- //Muutetaan annetun henkilön id numero-tyypiksi
-  const id = Number(request.params.id) 
-  const person = persons.find(per => per.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    //Jos kyseistä henkilöä ei löydy annetaan virhekoodi 404
-    response.status(404).end() 
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+        if(person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+        .catch(error => next(error))
 })
 
 // http://localhost:3001/info - sivu
@@ -60,19 +68,19 @@ app.get('/info', (request, response) => {
 })
 
 // Yksittäisen henkilötietojen poisto
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(per => per.id !== id)
-  
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-  
-// Apufunktio uuden id:n luomiselle
-const generateId = (max) => {
+// Apufunktio uuden id:n luomiselle (ennen MONGODB:n käyttöön ottamista)
+/* const generateId = (max) => {
     return Math.floor(Math.random() * Math.floor(max))
-}
+} */
 
-//Apufunktio nimen tarkistusta varten
+/* //Apufunktio nimen tarkistusta varten (ennen MONGODB:n käyttöön ottamista)
 const checkName = (name) => {
     //Kerätään nimet listaan tarkistamista varten
     let apu = []
@@ -81,35 +89,57 @@ const checkName = (name) => {
     }
     //Tarkistetaan onko nimi jo listassa
     return apu.includes(name)
-}
+} */
 
 //Uuden henkilön lisäys
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    
     if(!body.number || !body.name){
-        return response.status(400).json({ 
-            error: 'name or number is missing' 
-          })
-    }
-
-    const person = {
-      id: generateId(100),
-      name: body.name,
-      number: body.number
-    }
-  
-    if (checkName(person.name)){
         return response.status(400).json({
-            error: 'name must be unique'
+            error: 'name or number is missing'
         })
     }
 
-    persons = persons.concat(person)
-    response.json(person)
+    const person = new Person({
+        //id: generateId(100),
+        name: body.name,
+        number: body.number
+    })
+
+    person.save().then (savedPerson => {
+        response.json(savedPerson)
+    })
+
+    /* if (checkName(person.name)){
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    } */
 })
 
-const PORT = 3001
+//Virheiden käsittely middlewareina
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+//Olemattomat osoitteet
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error:'malform id' })
+    } else if (error.name === 'ValidationError'){
+        return response.status(400).json({ error : error.message })
+    }
+    next(error)
+}
+//Virheelliset pyynnöt
+app.use(errorHandler)
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-undef */
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.t9xfx.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+// Jos on annettu enemmän argumentteja kuin vain salasana
+if (process.argv.length > 3){
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    person.save().then(() => {
+        console.log(`added ${person.name} number ${person.number} to the phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+//Tulostetaan henkilöt ja numerot jos argumentiksi annetaan vain salasana
+if (process.argv.length === 3){
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
